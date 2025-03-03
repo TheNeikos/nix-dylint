3f71fc976d470e33570054022bbd736f5ce70c4f
Marcel Müller <neikos@neikos.email>
2025-03-03 10:08:09 +0100

    Add initial layout
    Signed-off-by: Marcel Müller <neikos@neikos.email>

--- flake.nix
+++ flake.nix
@@ -4,6 +4,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    crane.url = "github:ipetkov/crane";
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -19,8 +20,12 @@
           inherit system;
           overlays = [ inputs.rust-overlay.overlays.default ];
         };
+
+        lib = import ./lib { inherit inputs; };
       in
       {
+        inherit lib;
+
         devShells.default = pkgs.mkShell {
           nativeBuildInputs = [
             pkgs.rust-bin.nightly.latest.default
